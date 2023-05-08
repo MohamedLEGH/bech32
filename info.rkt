130faccd221dd8ccba324c841aa684f1da9c8648
Mohamed Amine LEGHERABA <mlegheraba@protonmail.com>
2023-05-08 02:22:49 +0200

    fix typo

--- info.rkt
+++ info.rkt
@@ -1,9 +1,9 @@
 #lang info
-(define collection "base58check")
+(define collection "bech32")
 (define deps '("base"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
-(define scribblings '(("scribblings/base58check.scrbl" ())))
-(define pkg-desc "Package to encode and decode base58check encoding")
+(define scribblings '(("scribblings/bech32.scrbl" ())))
+(define pkg-desc "Package to encode and decode bech32 encoding")
 (define version "0.0.1")
 (define pkg-authors '(Mohamed Amine LEGHERABA))
 (define license 'MIT)
